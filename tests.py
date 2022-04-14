adb12987155f5b5dd9430193636f8e9ba2d9fb4e
Yucheam <k.yucheam@yahoo.com>
2022-04-13 23:23:09 -0400

    Setting up project structure

---
+++ tests.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_numeric_sorting():
+    pass
+
+def test_alpha_sorting():
+    pass
+
+def test_alpha_and_numeric_sorting():
+    pass
+
+def test_csv_parsing():
+    pass
